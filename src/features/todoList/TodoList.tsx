92f8e53fbd2480ef51d0428e2d7f9540e782e544
HironoriSato <maza.hs5360@gmail.com>
2020-06-01 20:53:08 +0900

    [mod]todolistをフィルタリングして表示する機能の追加

--- src/features/todoList/TodoList.tsx
+++ src/features/todoList/TodoList.tsx
@@ -3,10 +3,27 @@ import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "app/rootReducer";
 import TodoListItem from "features/todoList/TodoListItem";
 import { toggleTodo } from "./todoSlice";
+import { Todo } from "./types";
+import { VisiblityFilter } from "features/visibilityFilter/visibilityFilterSlice";
+
+const getVisibleTodos = (todos: Todo[], filter: VisiblityFilter): Todo[] => {
+  switch (filter) {
+    case VisiblityFilter.ShowAll:
+      return todos;
+    case VisiblityFilter.ShowCompleted:
+      return todos.filter((todo) => todo.completed);
+    case VisiblityFilter.ShowActive:
+      return todos.filter((todo) => !todo.completed);
+    default:
+      throw new Error("Unknow filter: " + filter);
+  }
+};
 
 const TodoList = (): JSX.Element => {
   const dispatch = useDispatch();
-  const todos = useSelector((state: RootState) => state.todos);
+  const todos = useSelector((state: RootState) =>
+    getVisibleTodos(state.todos, state.visiblityFilter)
+  );
 
   return (
     <ul>
