20ce542a96cce6073dad43be79e0bd34d28d9191
HironoriSato <maza.hs5360@gmail.com>
2020-06-01 20:33:11 +0900

    [mod]todoの状態をトグルする機能の追加

--- src/features/todoList/TodoList.tsx
+++ src/features/todoList/TodoList.tsx
@@ -1,15 +1,21 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "app/rootReducer";
 import TodoListItem from "features/todoList/TodoListItem";
+import { toggleTodo } from "./todoSlice";
 
 const TodoList = (): JSX.Element => {
+  const dispatch = useDispatch();
   const todos = useSelector((state: RootState) => state.todos);
 
   return (
     <ul>
       {todos.map((todo) => (
-        <TodoListItem key={todo.id} {...todo} />
+        <TodoListItem
+          key={todo.id}
+          {...todo}
+          onClick={() => dispatch(toggleTodo(todo))}
+        />
         // <TodoListItem key={todo.id} id={todo.id} completed={todo.completed} text={todo.text} />
       ))}
     </ul>
--- src/features/todoList/TodoListItem.tsx
+++ src/features/todoList/TodoListItem.tsx
@@ -3,11 +3,13 @@ import React from "react";
 interface TodoProps {
   completed: boolean;
   text: string;
+  onClick: () => any;
 }
 
-const TodoListItem = ({ completed, text }: TodoProps) => {
+const TodoListItem = ({ completed, text, onClick }: TodoProps) => {
   return (
     <li
+      onClick={onClick}
       style={{
         textDecoration: completed ? "line-through" : "none",
       }}
