bcc025ef0caff85d2dd6d438f1622dd1bda9b490
HironoriSato <maza.hs5360@gmail.com>
2020-06-01 22:06:41 +0900

    [mod]

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import AddTodo from "features/todoList/AddTodo";
 import TodoList from "features/todoList/TodoList";
 import Fotter from "features/visibilityFilter/Footer";
 import "./App.css";
+import { useDispatch } from "react-redux";
+import { createTodoList, loadTodos } from "features/todoList/todoSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (window.location.pathname === "/") {
+      dispatch(createTodoList());
+    } else {
+      dispatch(loadTodos());
+    }
+  }, [dispatch]);
+  
   return (
     <div>
       <AddTodo />
