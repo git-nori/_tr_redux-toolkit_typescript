9b264db00e8b7484d2e50dfa4957fcbcbc091c33
HironoriSato <maza.hs5360@gmail.com>
2020-06-01 18:48:23 +0900

    [add]visiblityFilterSliceの実装

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import todos from "features/todoList/todoSlice";
+import visiblityFilter from "features/visibilityFilter/visibilityFilterSlice";
 
 const rootReducer = combineReducers({
   todos,
+  visiblityFilter,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
